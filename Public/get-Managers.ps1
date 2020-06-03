68b1ce5ef0bef487936ffe2d6d51b7e327145f9d
Dave Bremer <dave@bremer.co.nz>
2020-06-03 15:20:53 +1200

    get-managers

---
+++ Public/get-Managers.ps1
@@ -0,0 +1,64 @@
+﻿function Get-Managers {
+<#
+.SYNOPSIS
+	Lists the reporting chain for a person
+	
+.DESCRIPTION
+	lists as name, manager pairs the reporting chain
+	
+	
+.PARAMETER Identity
+    the username or Distinguished name for a person
+	
+	
+.EXAMPLE
+ get-managers myname123
+
+ name           Manager       
+----           -------       
+Alice Abrims   Bob Black
+Bob Black      Chris Clod
+Chris Clod     Dave Davis
+Dave Davis     Edgar Evans
+
+.NOTES	
+Author: Dave Bremer
+Date: 2020/6/3
+
+#todo
+It'd be better to have a single record spit out with one field being a collectio/array of managers
+then take an array of inputs
+	
+#>
+#Requires -Modules ActiveDirectory
+
+[CmdletBinding()]
+PARAM (
+	[Parameter(Mandatory)]
+	[String]$Identity,
+	[Switch]$lineManager
+)
+BEGIN {
+    
+
+}
+
+PROCESS
+{
+	foreach ($Account in $Identity)
+	{
+        $thisuser = get-aduser -identity $account -Properties manager
+        while ($thisuser.manager){
+           $thisuser | select name,@{n="Manager";e={$_.Manager -replace "(CN=)(.*?),.*",'$2'}}
+           if (-not $linemanager) { 
+                $thisuser = get-aduser -identity $thisuser.manager -Properties manager
+           } else {
+                break
+           }
+        }
+    }#foreach account
+		
+
+} #process
+END{}
+}
